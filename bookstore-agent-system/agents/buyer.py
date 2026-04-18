2c9350e28f6f6a84220e54b111eba0f9f6278439
BavanaTaviti <tavitibavana@gmail.com>
2026-04-18 23:34:38 +0530

    Delete bookstore-agent-system directory

--- bookstore-agent-system/agents/buyer.py
+++
@@ -1,9 +0,0 @@
-class BuyerAgent:
-    def __init__(self):
-        self.max_budget = 1000
-
-    def make_offer(self, current_price):
-        return current_price - 50
-
-    def final_decision(self, price, trust):
-        return price <= self.max_budget and trust > 0.6
